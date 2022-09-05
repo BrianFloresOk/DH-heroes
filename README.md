a5c93d2ddbd649674d21361472e5b2299edd0015
Brian Flores <brian.flores.ch@gmail.com>
2022-09-05 19:22:44 -0300

    Tp DH heroes

--- README.md
+++
@@ -1,4 +0,0 @@
-# TP-practica-de-express
-Trabajo practico "DH Heroes" de Playground
-Trabajo realizado por Brian Flores.
-Comision 13
--- app.js
+++ app.js
@@ -1,20 +1,54 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const PORT = 3030
 const path = require('path')
-const app = express();
-const PORT = 3030;
 
 
-app.use(express.static('img'))
+app.use(express.static("public"))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/Views/index.html')))
-app.get('/babbage', (req, res) => res.sendFile(path.join(__dirname, '/Views/babbage.html')))
-app.get('/berners-lee', (req, res) => res.sendFile(path.join(__dirname, '/Views/berners-lee.html')))
-app.get('/clarke', (req, res) => res.sendFile(path.join(__dirname, '/Views/clarke.html')))
-app.get('/hamilton', (req, res) => res.sendFile(path.join(__dirname, '/Views/hamilton.html')))
-app.get('/hopper', (req, res) => res.sendFile(path.join(__dirname, '/Views/hopper.html')))
-app.get('/lovelace', (req, res) => res.sendFile(path.join(__dirname, '/Views/lovelace.html')))
-app.get('/turing', (req, res) => res.sendFile(path.join(__dirname, '/Views/turing.html')))
+/* INDEX */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+})
+
+/* BABBAGE */
+app.get('/babbage', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/babbage.html"))
+})
+
+/* BENERS-LEE */
+app.get('/berners-lee', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/berners-lee.html"))
+})
+
+/* CLARKE */
+app.get('/clarke', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/clarke.html"))
+})
+
+/* HAMILTON */
+app.get('/hamilton', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/hamilton.html"))
+})
+
+/* HOPPER */
+app.get('/hopper', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/hopper.html"))
+})
+
+/* LOVELACE */
+app.get('/lovelace', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/lovelace.html"))
+})
+
+/* TURING */
+app.get('/turing', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/turing.html"))
+})
+
+app.get('/404', (req, res) => res.send('Error página no encontrada'))
 
 
-
-app.listen(PORT, () => console.log("Server On"));
+app.listen(PORT, () => {
+    console.log(`Servidor levantado http://localhost:${PORT}`);
+})
